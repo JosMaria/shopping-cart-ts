d1dd7f00ead17d47e84e961d46d8445edbd3ba58
JoseMaria <josmariaguilar@gmail.com>
2023-06-28 13:11:43 -0400

    fetch products

--- src/contexts/ProductsProvider.tsx
+++ src/contexts/ProductsProvider.tsx
@@ -1,4 +1,4 @@
-import { ReactElement, createContext, useState } from "react"
+import { ReactElement, createContext, useEffect, useState } from "react"
 
 export type ProductType = {
   sku: string
@@ -6,23 +6,7 @@ export type ProductType = {
   price: number
 }
 
-const initState: ProductType[] = [
-  {
-    sku: "item0001",
-    name: "Widget",
-    price: 9.99
-  },
-  {
-    sku: "item0002",
-    name: "Premium Widget",
-    price: 19.99
-  },
-  {
-    sku: "item0003",
-    name: "Deluxe Widget",
-    price: 29.99
-  }
-]
+const initState: ProductType[] = []
 
 export type UseProductsContextType = { products: ProductType[] }
 
@@ -33,10 +17,24 @@ const ProductsContext = createContext<UseProductsContextType>(initContextState)
 type ChildrenType = { children?: ReactElement | ReactElement[] }
 
 export const ProductsProvider = ({ children }: ChildrenType): ReactElement => {
-  const [products] = useState<ProductType[]>(initState)
-  
+  const [products, setProducts] = useState<ProductType[]>(initState)
+  useEffect(() => {
+    const fetchProducts = async (): Promise<ProductType[]> => {
+      const data = await fetch('http://localhost:3500/products')
+        .then(response => response.json())
+        .catch(error => {
+          if (error instanceof Error) 
+            console.log(error)
+        })
+
+      return data
+    }
+
+    fetchProducts()
+      .then(products => setProducts(products))
+  }, [])
   return (
-    <ProductsContext.Provider value={{products}}>
+    <ProductsContext.Provider value={{ products }}>
       {children}
     </ProductsContext.Provider>
   )
